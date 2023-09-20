e8d985bf141adb9e80916ee6227e6446e831f12c
Liszt-Fly <1904231450@qq.com>
2023-09-21 01:44:05 +0800

    登录功能

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LoginForm } from './dto/LoginForm';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Post('api')
+  getHello(@Body() loginForm: LoginForm): string {
+    console.log(loginForm);
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FileController } from './FilesController';
-import { GenerateJsonService } from './generate-json/generate-json.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, FileController],
-  providers: [AppService, GenerateJsonService],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
