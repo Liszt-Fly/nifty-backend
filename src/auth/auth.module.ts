b85798105806f5de43df937a23023dd103d3e943
Liszt-Fly <1904231450@qq.com>
2023-10-02 19:28:32 +0800

    update

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,10 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { EmailModule } from 'src/email/email.module';
 import { EmailService } from 'src/email/email.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
-import { PrismaService } from 'src/prisma/prisma.service';
 @Module({
   imports: [
     ConfigModule,
@@ -18,7 +18,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
       useFactory: (config: ConfigService) => {
         return {
           secret: config.get('TOKEN_SECRET'),
-          signOptions: { expiresIn: '30d' },
+          signOptions: { expiresIn: '7d' },
         };
       },
     }),
